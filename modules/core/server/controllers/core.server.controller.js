3d6b2f2ff34ab18f95059d1282a647fef092cd4f
ahulshult <ahulshult@ufl.edu>
2017-11-14 18:33:07 -0500

    contact form created that sends message to gmail

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -1,5 +1,12 @@
 'use strict';
+var path = require('path'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  passport = require('passport'),
+  config = require(path.resolve('./config/config')),
+  nodemailer = require('nodemailer');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+var smtpTransport = nodemailer.createTransport(config.mailer.options);
 /**
  * Render the main application page
  */
@@ -37,6 +44,30 @@ exports.renderNotFound = function (req, res) {
     },
     'default': function () {
       res.send('Path not found');
+    }
+  });
+};
+
+exports.sendContactForm = function (req, res) {
+  //var emailhtml = undefined;
+  console.log(req.body);
+  var message = '---------------------------\n \n' + req.body.message + '\n \n From, \n' + req.body.studentName + '\n ---------------------------- \n \n Please respond to this person with the following email: \n' + req.body.studentEmail;
+  var mailOptions={
+    to: config.mailer.from,
+    from: config.mailer.from,
+    subject: '[MIL]' + req.body.subject,
+    text: message
+  };
+  smtpTransport.sendMail(mailOptions, function (err) {
+    if (!err) {
+      res.send({
+        message: 'An email has been sent to the provided email with further instructions.'
+      });
+    } else {
+      console.log(err);
+      return res.status(400).send({
+        message: 'Failure sending email'
+      });
     }
   });
 };
